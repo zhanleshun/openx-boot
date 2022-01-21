d3161272bd46d84c8f99d67d3687487767f27555
zhanleshun <zhanleshun@163.com>
2022-01-21 11:25:01 +0800

    init

---
+++ openx-boot-booster/src/test/java/openx/boot/booster/OpenxBootBoosterApplicationTests.java
@@ -0,0 +1,13 @@
+package openx.boot.booster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenxBootBoosterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
